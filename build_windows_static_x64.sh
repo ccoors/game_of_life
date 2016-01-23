4afbb3d170db2d8c0e25282c1709b27c71f731dc
Christian F. Coors <ccoors@uni-bremen.de>
2016-01-24 00:22:31 +0100

    Moved code to src/

--- build_windows_static_x64.sh
+++ build_windows_static_x64.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-x86_64-w64-mingw32-g++ -static -static-libgcc -static-libstdc++ -lpthread -Wall -O3 -std=c++14 -o main_x64.exe *.cpp
+x86_64-w64-mingw32-g++ -static -static-libgcc -static-libstdc++ -lpthread -Wall -O3 -std=c++14 -o main_x64.exe src/*.cpp
