88efc92e3aaf0069dba1bd409efcdaf25868df82
Christian F. Coors <ccoors@uni-bremen.de>
2016-01-23 19:58:40 +0100

    Added Scripts for Windows x86 and x64

--- build_windows_static.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-i686-w64-mingw32-g++ -static -static-libgcc -static-libstdc++ -lpthread -Werror -Wall -O3 -std=c++14 -o main.exe *.cpp
---
+++ build_windows_static_x64.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+x86_64-w64-mingw32-g++ -static -static-libgcc -static-libstdc++ -lpthread -Werror -Wall -O3 -std=c++14 -o main_x64.exe *.cpp
